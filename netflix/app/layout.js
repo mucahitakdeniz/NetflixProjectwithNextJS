f6e08b0c9c82586785bf35d56fa748a3d8e768aa
mucahit <mucahitakdeniz28@gmail.com>
2024-01-13 00:11:43 +0300

    up

--- netflix/app/layout.js
+++ netflix/app/layout.js
@@ -1,17 +1,28 @@
-import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import AuthContextProvider from "@/context/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+//! metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
 export const metadata = {
-  title: "Netfilix",
+  title: "Netflix",
   description: "a movie platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthContextProvider>
+          <Navbar />
+          {children}
+          <ToastContainer />
+        </AuthContextProvider>
+      </body>
     </html>
   );
 }
--- netflix/app/page.js
+++ netflix/app/page.js
@@ -1,6 +1,19 @@
+"use client";
+import { useAuthContext } from "@/context/AuthContext";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+//? app folder içindeki page.js "/" route olan home sayfasıdır
+
+//? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use-client" direktifini kullanmamız gerekir
 
 export default function Home() {
+  const { currentUser } = useAuthContext();
+  let router = useRouter();
+  useEffect(() => {
+    currentUser && router.push("/movies");
+  }, [currentUser]);
+
   return (
     <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full bg-opacity-50">
@@ -11,8 +24,9 @@ export default function Home() {
           <p className="text-2xl font-[400] ">
             Watch anywhere. Cancel anytime.
           </p>
+          {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
           <Link href="/register">
-            <button className="btn-danger w-[230px] ">Get Started</button>
+            <button className="btn-danger w-[250px]">Get Started</button>
           </Link>
         </div>
       </div>
